a27f1205d1a4775474aef01655cf971460042a29
rummaniit <rummanoffice12@gmail.com>
2022-01-27 19:21:54 +0600

    added files

---
+++ doubleeqandtripleeq.js
@@ -0,0 +1,17 @@
+const val = 0
+const val2 = false
+//as 0 and false same so result will be true
+// if (val == val2) {
+//     console.log('True')
+// }
+// else {
+//     console.log('False')
+// }
+
+//It will result false cz it will also check types
+if (val === val2) {
+    console.log('True')
+}
+else {
+    console.log('False')
+}
---
+++ mapfilters.js
@@ -0,0 +1,44 @@
+const elements = [3, 4, 5, 6]
+// const array = []
+// for (let i = 0; i < elements.length; i++) {
+//     const element = elements[i]
+//     const result = element * element
+//     array.push(result)
+// }
+// console.log(array)
+
+
+// const ans = element => element * element
+// let res = ans(5)
+// console.log(res);
+
+
+
+// const output = elements.map(function (ele) {
+//     return ele * ele
+// })
+// console.log(output);
+
+
+
+
+// const result = elements.map(element => element + 2)
+// console.log(result)
+
+// const bigger = elements.filter(x => x > 5)
+// console.log(bigger)
+
+const person = [
+    { id: 21, name: 'rumman' },
+    { id: 22, name: 'shaon' },
+    { id: 23, name: 'aminul' }
+]
+// let array = []
+// for (let i = 0; i < person.length; i++) {
+//     array.push(person[i].name)
+// }
+// console.log(array)
+const res = person.map(s => s.name)
+console.log(res)
+const res2 = person.map(x => x.id)
+console.log(res2)
